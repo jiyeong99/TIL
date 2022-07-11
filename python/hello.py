8c7ea897958e51184c9a4a2bad3bab23a1d37626
jiyeong99 <1999wldud@gmail.com>
2022-07-11 14:20:31 +0900

    파이썬추가

---
+++ python/hello.py
@@ -0,0 +1,23 @@
+'''
+print("Hello world"*5)
+'''
+
+'''
+print("Hello world")
+print("Hello world")
+print("Hello world")
+print("Hello world")
+print("Hello world")
+'''
+
+# hello = "Hello world"
+# print(hello)
+
+# a = 5
+# b = 3
+# print(a + b)
+
+a = "hello"
+print (a)
+# ㅍㅏ이썬은 위에서 아래로 읽는다
+
